84397f0e9298c186b373344b0454282421a91a33
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-12-02 15:22:17 -0500

    Continuing with let, const and var
    Left off at the let, const and var lecture. Adding code files/snippets for challenges/lessons as they happen.
    
    Need to containerize sections more cleanly.

---
+++ 01-Fundamentals-Part-1/starter/fundamentals_partOne.js
@@ -0,0 +1,21 @@
+
+//Lecture: Values and Variables
+
+let country = 'USA';
+let continent = 'North America';
+let population = 374340354;
+
+/*console.log(country);
+console.log(continent);
+console.log(population);*/
+
+
+// Lecture: Data Types
+
+let isIsland = true;
+let language;
+
+console.log(isIsland)
+console.log(country);
+console.log(population);
+console.log(language);
