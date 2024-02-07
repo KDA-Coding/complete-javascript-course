f22d3d3cfa0e7c94ef8596ae54ed2393f5172a40
KDA-Coding <59184838+KDA-Coding@users.noreply.github.com>
2024-02-07 16:06:45 -0500

    Continuing Operator Learning
    Continuing with learning about different kinds of operators in JS. Learned about Power Operator.
    
    Not certain what application use cases it has. Could be useful for tracking or creating relative exponential growth but in what way would that be useful to web development?

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -70,6 +70,31 @@ const lastName = 'Allgeyer';
 console.log(firstName + lastName);
 */
 
-const ageJonas = 2037 - 1991;
 
-console.log(ageJonas);
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2* 2 = 8
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1
+x--; //x = x - 1
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
