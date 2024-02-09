ccbf42ba9221c5a86514c6f471c9ad421ed8bda2
KDA-Coding <59184838+KDA-Coding@users.noreply.github.com>
2024-02-09 18:15:25 -0500

    Completed Lesson 13. Return to Operator Precedence Later

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -74,7 +74,7 @@ console.log(firstName + lastName);
 // Math Operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2020;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -98,3 +98,5 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
